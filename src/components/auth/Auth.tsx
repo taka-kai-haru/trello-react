7b08c41820b14407956a6145368f2887bcd9ef31
s_takahashi <erie.orchid.dev@gmail.com>
2023-09-23 13:34:35 +0900

    ログイン時のダイアログの表示内容を変更

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -90,7 +90,7 @@ export const Auth: FC = () => {
         errorMessage
           ? setDialogMessage(errorMessage)
           : setDialogMessage(error.message);
-        setDialogTitle("エラー");
+        setDialogTitle("警告");
         setOpenDialog(true);
       });
   };
